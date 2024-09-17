bd6504d2431000471f0d06542a26422cfcbcc776
KBuseDincer <104794675+KBuseDincer@users.noreply.github.com>
2024-09-17 16:10:03 +0300

    Create Etiya.sql

---
+++ Etiya.sql
@@ -0,0 +1,27 @@
+--SELECT * FROM Products 
+--WHERE supplierid BETWEEN 1 and 5;
+
+--select * from Products
+--where supplierid IN (1,2,4,5);
+
+--select * from Products
+--where productname IN('Chang','Aniseed Syrup');
+
+--Select * from Products
+--where supplierid = 3 OR unitprice>10;
+
+--SELECT productname, unitprice from Products;
+
+--select productname from Products
+--where productname LIKE '%C%';
+
+--SELECT productname from Products
+--where productname LIKE 'N%' AND productname NOT LIKE '%N%N%';
+
+--select * from Products
+--where unitsinstock>50;
+
+--select productname, MIN(unitprice), MAX(unitprice) from Products;
+
+--select productname from Products
+--where productname LIKE '%Spice%';
